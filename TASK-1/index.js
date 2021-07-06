eb37b691245405a6c32ec5eaa1b627184b956ad2
Deepak Verma <0822deepakverma@gmail.com>
2021-07-07 04:20:40 +0530

    Done

--- TASK-1/index.js
+++ TASK-1/index.js
@@ -18,7 +18,7 @@ $('.slider-for').slick({
     asNavFor: '.slider-nav'
   });
   $('.slider-nav').slick({
-    slidesToShow: 3,
+    slidesToShow: 4,
     slidesToScroll: 1,
     asNavFor: '.slider-for',
     dots: false,
